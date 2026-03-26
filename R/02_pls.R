e9c1f2fc999089d56e97f6421dc91a2c15b292c1
Massimo-ov <massimoarmano.ov@gmail.com>
2026-03-26 21:06:17 +0100

    Add PLS model implementation in 02_pls.R

---
+++ R/02_pls.R
@@ -0,0 +1,17 @@
+# PLS MODEL, RUN AFTER 01_preprocess_data.R
+pls_model <- plsr(y_train ~ X_train,
+                   ncomp = 30,
+                   validation = "CV")
+  
+  # automatic choice of the component
+  ncomp_pls <- which.min(RMSEP(pls_model)$val[1,1,])
+  
+  # Prediction
+  pls_pred <- predict(pls_model,
+                      newdata = X_test,
+                      ncomp = ncomp_pls)
+  loadings <- pls_model$loading.weights
+  
+  
+  # MSE test
+  mse_pls <- mean((y_test - pls_pred)^2)
